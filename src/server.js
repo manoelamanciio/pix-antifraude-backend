6dd1caf485774fcd4f3861b22cabe76cd4837878
manoelamanciio <manoel.amanciio@gmail.com>
2026-01-24 16:41:33 -0300

    feat: complete Pix antifraud backend with UI simulation

--- src/server.js
+++ src/server.js
@@ -3,10 +3,17 @@ import { receivePix, receiverConfirm } from "./controllers/pixController.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("🚀 Sistema Pix Antifraude está rodando");
+});
 
 app.post("/pix/receive", receivePix);
 app.post("/pix/receiver-decision", receiverConfirm);
 
-app.listen(3000, () => {
-  console.log("🚀 Sistema Pix Antifraude rodando na porta 3000");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Sistema Pix Antifraude rodando na porta ${PORT}`);
 });
